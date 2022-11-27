59cd1e345b15af20289e9813da40b6bff989ded7
Kancharla Sree Harsha <84336340+harshak02@users.noreply.github.com>
2022-11-27 20:10:23 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+//always remember that the components should be in block letters
+
+//self clsoing tag ->given down
+
+ReactDOM.render(
+  <div>
+    {/* <Heading></Heading> */}
+    <App />
+  </div>,
+  document.getElementById("root")
+);
